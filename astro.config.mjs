75d3478328682feb5e6fe40874d8241221f602bb
Chi Nguyen <c.nguyen.prog@gmail.com>
2026-03-30 01:05:41 +0200

    oops

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-
 import vue from "@astrojs/vue";
-
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
